49b8320375a350817a93f6938f0a49b8a9c9bb71
Ty Le <anhtuan.n.le@gmail.com>
2017-02-10 14:38:20 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -38,6 +38,14 @@ app.post("/todos", (req,res) => {
 	});
 });
 
+app.get("/todos", (req,res) =>{
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
 
 app.listen(3000, (req,res) =>{
 	console.log("Server Deployed!");
