2541227b62a69480cd18fbf19f0f91627c3e0ee7
Ty Le <anhtuan.n.le@gmail.com>
2017-02-10 19:11:03 -0500

    Updated mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,7 +8,7 @@ let db = {
   mlab: "mongodb://heyty:A1b2c3d4@ds147799.mlab.com:47799/todo_api_db"
 };
 
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect( db.localhost || db.mlab);
 
 
 module.exports = {
